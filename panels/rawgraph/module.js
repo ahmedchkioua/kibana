ca202b1d5ac9f3c853c8c2937f12c72c17c3f6b6
Benoit Dunand-Laisin <benoit.dunand-laisin@prologism.fr>
2013-08-02 16:03:35 +0200

    allow multi series in rawgraph

--- panels/rawgraph/module.js
+++ panels/rawgraph/module.js
@@ -15,10 +15,10 @@
   * interval :: Datapoint interval in elasticsearch date math format (eg 1d, 1w, 1y, 5y)
   * fill :: Only applies to line charts. Level of area shading from 0-10
   * linewidth ::  Only applies to line charts. How thick the line should be in pixels
-                  While the editor only exposes 0-10, this can be any numeric value. 
+                  While the editor only exposes 0-10, this can be any numeric value.
                   Set to 0 and you'll get something like a scatter plot
   * timezone :: This isn't totally functional yet. Currently only supports browser and utc.
-                browser will adjust the x-axis labels to match the timezone of the user's 
+                browser will adjust the x-axis labels to match the timezone of the user's
                 browser
   * spyable ::  Dislay the 'eye' icon that show the last elasticsearch query
   * zoomlinks :: Show the zoom links?
@@ -42,7 +42,7 @@ angular.module('kibana.rawgraph', [])
     query       : [ {query: "*", label:"Query"} ],
     max_point   : 5000,
     time_field  : '@timestamp',
-    value_field : null,
+    series : [ {value_field : null, hide: false} ],
     fill        : 0,
     linewidth   : 1,
     timezone    : 'browser', // browser, utc or a standard timezone
@@ -101,7 +101,7 @@ angular.module('kibana.rawgraph', [])
         .postTags('@end-highlight@')
       )
       .size($scope.panel.max_point)
-      .sort($scope.panel.time_field,"desc");;
+      .sort($scope.panel.time_field, "desc");
 
     // Populate the inspector panel
     $scope.populate_modal(request);
@@ -133,52 +133,64 @@ angular.module('kibana.rawgraph', [])
             highlight : kbn.flatten_json(hit.highlight||{})
           };
         }));
-        
+
         $scope.hits += results.hits.total;
-        $scope.truehits = 0;
-        var i = 0;
-        
-        // Create the flot series object
+
+        // Create the flot series object for visible series
         $scope.plotseries = [];
-        $scope.plotseries[0] = {
-            data: [],
-            hits: 0,
-            info: $scope.queries.list[0]
-        };
-        $scope.plotseries[0].info.alias=$scope.panel.value_field;
-        // Get printed value
-        _.each($scope.data, function(item, index) {
-          // pas convaincu de cette manière de récupérer la valeur
-          if (typeof item._source[$scope.panel.value_field] !== 'undefined' && item._source[$scope.panel.value_field] !== null) {
-            // Timestamp parsing
-            var parts = item._source[$scope.panel.time_field].split('T'),
-            dateParts = parts[0].split('-'),
-            timeParts = parts[1].split('Z'),
-            timeSubParts = timeParts[0].split(':'),
-            timeSecParts = timeSubParts[2].split('.'),
-            _date = new Date;
-            _date.setUTCFullYear(Number(dateParts[0]));
-            _date.setUTCMonth(Number(dateParts[1])-1);
-            _date.setUTCDate(Number(dateParts[2]));
-            _date.setUTCHours(Number(timeSubParts[0]));
-            _date.setUTCMinutes(Number(timeSubParts[1]));
-            _date.setUTCSeconds(Number(timeSecParts[0]));
-            if (timeSecParts[1]) _date.setUTCMilliseconds(Number(timeSecParts[1]));
-            
-            $scope.plotseries[0].data[i] = [_date.getTime(), item._source[$scope.panel.value_field]];
-            $scope.plotseries[0].hits += 1;
-            i++;
+        _.each($scope.panel.series, function(item, index) {
+          if (!item.hide) {
+            $scope.plotseries.push({
+                data: [],
+                hits: 0,
+                info: {
+                  alias:item.value_field,
+                  color: $scope.queries.colors[index],
+                }
+            });
           }
         });
-        $scope.truehits=i;
-        
+
+        // Get data and assign in their own series
+        _.each($scope.data, function(item_data, index_data) {
+          // Not convinced by that
+          var cached_time_field=null;
+          _.each($scope.plotseries, function(item_series, index_series) {
+            if (typeof item_data._source[item_series.info.alias] === 'number'
+             && typeof item_data._source[$scope.panel.time_field] !== 'undefined' && item_data._source[$scope.panel.time_field] !== null ) {
+              if (cached_time_field === null)
+              {
+                // Timestamp parsing
+                var parts = item_data._source[$scope.panel.time_field].split('T'),
+                dateParts = parts[0].split('-'),
+                timeParts = parts[1].split('Z'),
+                timeSubParts = timeParts[0].split(':'),
+                timeSecParts = timeSubParts[2].split('.'),
+                _date = new Date;
+                _date.setUTCFullYear(Number(dateParts[0]));
+                _date.setUTCMonth(Number(dateParts[1])-1);
+                _date.setUTCDate(Number(dateParts[2]));
+                _date.setUTCHours(Number(timeSubParts[0]));
+                _date.setUTCMinutes(Number(timeSubParts[1]));
+                _date.setUTCSeconds(Number(timeSecParts[0]));
+                if (timeSecParts[1]) _date.setUTCMilliseconds(Number(timeSecParts[1]));
+
+                cached_time_field=_date.getTime()
+              }
+
+              $scope.plotseries[index_series].data.push([cached_time_field, item_data._source[item_series.info.alias]]);
+              $scope.plotseries[index_series].hits += 1;
+            }
+          });
+        });
+
         // Tell the histogram directive to render.
         $scope.$emit('render');
-        
+
       } else {
         return;
       }
-      
+
     });
   };
 
@@ -209,7 +221,7 @@ angular.module('kibana.rawgraph', [])
       to:moment.utc(_to),
       field:$scope.panel.time_field
     });
-    
+
     dashboard.refresh();
   };
 
@@ -220,12 +232,12 @@ angular.module('kibana.rawgraph', [])
       body : "<h5>Last Elasticsearch Query</h5><pre>"+
           'curl -XGET '+config.elasticsearch+'/'+dashboard.indices+"/_search?pretty -d'\n"+
           angular.toJson(JSON.parse(request.toString()),true)+
-        "'</pre>", 
-    }; 
+        "'</pre>",
+    };
   };
-  
-  $scope.set_refresh = function (state) { 
-    $scope.refresh = state; 
+
+  $scope.set_refresh = function (state) {
+    $scope.refresh = state;
   };
 
   $scope.close_edit = function() {
@@ -234,6 +246,16 @@ angular.module('kibana.rawgraph', [])
     }
     $scope.refresh =  false;
     $scope.$emit('render');
+  };
+
+  $scope.add_series = function () {
+
+    var new_series = {
+      value_field: null,
+      hide: false
+    };
+    $scope.panel.series.push(new_series);
+    $scope.refresh =  true;
   };
 
 })
@@ -247,7 +269,7 @@ angular.module('kibana.rawgraph', [])
       scope.$on('render',function(){
         render_panel();
       });
-  
+
       // Re-render if the window is resized
       angular.element(window).bind('resize', function(){
         render_panel();
@@ -259,34 +281,42 @@ angular.module('kibana.rawgraph', [])
         // IE doesn't work without this
         elem.css({height:scope.panel.height||scope.row.height});
 
+        if (scope.plotseries.length === 0) {
+            elem.text("No series to draw for the moment");
+            return;
+        }
+
         // Populate from the query service
         try {
           _.each(scope.plotseries,function(series) {
             series.label = series.info.alias;
             series.color = series.info.color;
           });
-        } catch(e) {return;}
+        } catch(e) {
+            elem.text("Something is wrong about alias series or color series");
+            return;
+        }
 
         var scripts = $LAB.script("common/lib/panels/jquery.flot.js").wait()
           .script("common/lib/panels/jquery.flot.time.js")
           .script("common/lib/panels/jquery.flot.stack.js")
           .script("common/lib/panels/jquery.flot.selection.js")
           .script("common/lib/panels/timezone.js");
-                    
+
         // Populate element. Note that jvectormap appends, does not replace.
         scripts.wait(function(){
           var stack = scope.panel.stack ? true : null;
 
           // Populate element
-          try { 
+          try {
             var options = {
               legend: { show: false },
               series: {
                 stackpercent: scope.panel.stack ? scope.panel.percentage : false,
                 stack: scope.panel.percentage ? null : stack,
-                lines:  { 
-                  show: scope.panel.lines, 
-                  fill: scope.panel.fill/10, 
+                lines:  {
+                  show: scope.panel.lines,
+                  fill: scope.panel.fill/10,
                   lineWidth: scope.panel.linewidth,
                   steps: false
                 },
@@ -294,11 +324,11 @@ angular.module('kibana.rawgraph', [])
                 points: { show: scope.panel.points, fill: 1, fillColor: false, radius: 5},
                 shadowSize: 1
               },
-              yaxis: { 
-                show: scope.panel['y-axis'], 
-                min: 0, 
-                max: scope.panel.percentage && scope.panel.stack ? 100 : null, 
-                color: "#c8c8c8" 
+              yaxis: {
+                show: scope.panel['y-axis'],
+                min: 0,
+                max: scope.panel.percentage && scope.panel.stack ? 100 : null,
+                color: "#c8c8c8"
               },
               xaxis: {
                 timezone: scope.panel.timezone,
@@ -323,7 +353,7 @@ angular.module('kibana.rawgraph', [])
             }
 
             scope.plot = $.plot(elem, scope.plotseries, options);
-            
+
             // Work around for missing legend at initialization.
             if(!scope.$$phase) {
               scope.$apply();
@@ -337,7 +367,7 @@ angular.module('kibana.rawgraph', [])
 
       function tt(x, y, contents) {
         // If the tool tip already exists, don't recreate it, just update it
-        var tooltip = $('#pie-tooltip').length ? 
+        var tooltip = $('#pie-tooltip').length ?
           $('#pie-tooltip') : $('<div id="pie-tooltip"></div>');
 
         tooltip.html(contents).css({
@@ -358,7 +388,7 @@ angular.module('kibana.rawgraph', [])
           tt(pos.pageX, pos.pageY,
             "<div style='vertical-align:middle;display:inline-block;background:"+
             item.series.color+";height:15px;width:15px;border-radius:10px;'></div> "+
-            item.datapoint[1].toFixed(0) + " @ " + 
+            item.datapoint[1].toFixed(0) + " @ " +
             moment(item.datapoint[0]).format('MM/DD HH:mm:ss'));
         } else {
           $("#pie-tooltip").remove();
